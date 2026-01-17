373ffd7f01ad2d03209514f4229ee95a3e7116e0
sofi-mcc057 <princesssofia2006@gmail.com>
2026-01-17 16:41:29 -0500

    added function to make a review

--- mongo_db.py
+++ mongo_db.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import json
 
 uri = "mongodb+srv://sofi-mcc057:sOfIa2006@cluster0.8ndz01r.mongodb.net/?appName=Cluster0"
 
@@ -37,11 +38,11 @@ def trigger_overall_ratings(dormID):
         total_academic += r.get("academicProximity", 0)
         total_amenities += r.get("amenities", 0)
 
-    avg_room = total_room / review_count
-    avg_dining = total_dining / review_count
-    avg_academic = total_academic / review_count
-    avg_amenities = total_amenities / review_count
-    overall_rating = (avg_room + avg_dining + avg_academic + avg_amenities) / 4
+    avg_room = total_room // review_count
+    avg_dining = total_dining // review_count
+    avg_academic = total_academic // review_count
+    avg_amenities = total_amenities // review_count
+    overall_rating = (avg_room + avg_dining + avg_academic + avg_amenities) // 4
 
     overall_ratings.update_one(
         {"dormID": dormID},
@@ -59,3 +60,17 @@ def trigger_overall_ratings(dormID):
     )
 
 trigger_overall_ratings(4)
+
+test = {
+        "roomSize": 1,
+        "diningProximity": 1,
+        "academicProximity": 1,
+        "amenities": 1,
+        "comment": "it's fine",
+        "dormID": 4
+    }
+
+def json_to_review(file):
+    reviews.insert_one(file)
+
+json_to_review(test)
