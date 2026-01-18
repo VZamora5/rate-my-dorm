ebd5b31f4ba3715725d780f80fd101880c46162a
Flippyfun02 <amiebinan@gmail.com>
2026-01-18 03:41:01 -0500

    changed routes

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("search.html", {"request": request})
 
 @app.get("/login", response_class=HTMLResponse)
 async def add_review(request: Request):
